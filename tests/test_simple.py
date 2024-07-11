ec16f7e3d4ac48aba31a6e290c4547aad29b9a1a
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2024-07-11 13:34:32 +0200

    tests: simple test that pay two nodes
    This is implementing a proof of work of the plugin
    with the core lightning integration testing.
    
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,6 +1,8 @@
 import os
 
 from pyln.testing.fixtures import *  # noqa: F403
+from pyln.testing.utils import only_one
+from pyln.client import Millisatoshi
 
 barq_binary = os.path.join(os.path.dirname(__file__), "../target/debug/barq-plugin")
 
@@ -14,3 +16,34 @@ def test_init(node_factory):
         }
     )
     assert True
+
+
+def test_pay_amounts(node_factory):
+    """We steal this from core lightning test_pay.py, and we are try just 
+    to pay and invoice with amount"""
+    l1, l2 = node_factory.line_graph(2, opts=[{"plugin": barq_binary }, { "plugin": barq_binary}])
+    inv = l2.rpc.invoice(Millisatoshi("123sat"), 'test_pay_amounts', 'description')['bolt11']
+
+    invoice = only_one(l2.rpc.listinvoices('test_pay_amounts')['invoices'])
+
+    assert invoice['amount_msat'] == Millisatoshi(123000)
+
+    l1.rpc.call("barqpay", {"bolt11_invoice": inv})
+
+    invoice = only_one(l2.rpc.listinvoices('test_pay_amounts')['invoices'])
+    assert invoice['amount_received_msat'] >= Millisatoshi(123000)
+
+
+def test_pay_without_amounts(node_factory):
+    """We are going to test that we can pay an invoice without amount"""
+    l1, l2 = node_factory.line_graph(2)
+    inv = l2.rpc.invoice("any", 'test_pay_amounts', 'description')['bolt11']
+
+    invoice = only_one(l2.rpc.listinvoices('test_pay_amounts')['invoices'])
+
+    assert invoice['amount_msat'] == Millisatoshi(123000)
+
+    l1.rpc.call("barqpay", {"bolt11_invoice": inv})
+
+    invoice = only_one(l2.rpc.listinvoices('test_pay_amounts')['invoices'])
+    assert invoice['amount_received_msat'] >= Millisatoshi(123000)

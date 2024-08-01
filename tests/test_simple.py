2f6ab6b9ac7f3714f6ce009449a6d989f387dd52
raehat <raehatsinghnanda@gmail.com>
2024-08-02 02:21:25 +0530

    Fix tests: - Add wait_for_announce=True to ensure `listchannels` is updated - Assert if invoice is paid in tests
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -21,7 +21,7 @@ def test_init(node_factory):
 def test_pay_amounts(node_factory):
     """We steal this from core lightning test_pay.py, and we are try just 
     to pay and invoice with amount"""
-    l1, l2 = node_factory.line_graph(2, opts=[{"plugin": barq_binary }, { "plugin": barq_binary}])
+    l1, l2 = node_factory.line_graph(2, opts=[{"plugin": barq_binary }, { "plugin": barq_binary}], wait_for_announce=True)
     inv = l2.rpc.invoice(Millisatoshi("123sat"), 'test_pay_amounts', 'description')['bolt11']
 
     invoice = only_one(l2.rpc.listinvoices('test_pay_amounts')['invoices'])
@@ -32,18 +32,17 @@ def test_pay_amounts(node_factory):
 
     invoice = only_one(l2.rpc.listinvoices('test_pay_amounts')['invoices'])
     assert invoice['amount_msat'] >= Millisatoshi(123000)
+    assert invoice['status'] == 'paid'
 
 
 def test_pay_without_amounts(node_factory):
     """We are going to test that we can pay an invoice without amount"""
-    l1, l2 = node_factory.line_graph(2)
+    l1, l2 = node_factory.line_graph(2, opts=[{"plugin": barq_binary }, { "plugin": barq_binary}], wait_for_announce=True)
     inv = l2.rpc.invoice("any", 'test_pay_amounts', 'description')['bolt11']
 
     invoice = only_one(l2.rpc.listinvoices('test_pay_amounts')['invoices'])
 
-    assert invoice['amount_msat'] == Millisatoshi(123000)
-
-    l1.rpc.call("barqpay", {"bolt11_invoice": inv})
+    l1.rpc.call("barqpay", {"bolt11_invoice": inv, "amount_msat": 123000})
 
     invoice = only_one(l2.rpc.listinvoices('test_pay_amounts')['invoices'])
-    assert invoice['amount_msat'] >= Millisatoshi(123000)
+    assert invoice['status'] == 'paid'

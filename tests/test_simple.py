7c7c87610a7302bd4ce0b7470c6ac05d8d797835
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2024-08-02 11:58:01 +0200

    tests: make sure that we raise an error when there is no route
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -3,6 +3,7 @@ import os
 from pyln.testing.fixtures import *  # noqa: F403
 from pyln.testing.utils import only_one
 from pyln.client import Millisatoshi
+from pyln.client import RpcError
 
 barq_binary = os.path.join(os.path.dirname(__file__), "../target/debug/barq-plugin")
 
@@ -42,7 +43,39 @@ def test_pay_without_amounts(node_factory):
 
     invoice = only_one(l2.rpc.listinvoices('test_pay_amounts')['invoices'])
 
+    # we fail when there is no amount inside the invoice and we do not
+    # specify the amount inside the pay command
+    with pytest.raises(RpcError):
+        l1.rpc.call("barqpay", {"bolt11_invoice": inv})
     l1.rpc.call("barqpay", {"bolt11_invoice": inv, "amount_msat": 123000})
 
     invoice = only_one(l2.rpc.listinvoices('test_pay_amounts')['invoices'])
     assert invoice['status'] == 'paid'
+
+
+def test_pay_fail_when_there_is_no_channel(node_factory):
+    """We make sure that our is not able to pay an invoice when there is no channel"""
+    l1 = node_factory.get_node(
+        options={
+            "plugin": barq_binary,
+        }
+    )
+    l2 = node_factory.get_node(
+        options={
+            "plugin": barq_binary,
+        }
+    )
+
+    inv = l2.rpc.invoice(Millisatoshi("123sat"), 'test_pay_amounts', 'description')['bolt11']
+
+    invoice = only_one(l2.rpc.listinvoices('test_pay_amounts')['invoices'])
+
+    assert invoice['amount_msat'] == Millisatoshi(123000)
+
+    # we fail because there is no channel
+    with pytest.raises(RpcError):
+        l1.rpc.call("barqpay", {"bolt11_invoice": inv})
+
+    # Sanity check
+    invoice = only_one(l2.rpc.listinvoices('test_pay_amounts')['invoices'])
+    assert invoice['status'] == 'unpaid'

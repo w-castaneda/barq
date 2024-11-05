dd643ee28f276f7b2ce00fe577c5c59454ab1ce3
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2024-11-05 17:05:40 +0100

    comm: fixing the fee calculation
    Suggested-by: https://github.com/coffee-tools/barq/pull/45/files#r1740192735
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- barq-common/src/algorithms/direct.rs
+++ barq-common/src/algorithms/direct.rs
@@ -85,10 +85,7 @@ impl Strategy for Direct {
             input.dest_pubkey.clone(),
             channel.short_channel_id.clone(),
             input.cltv as u32,
-            // FIXME: Double check for this?
-            input.amount_msat
-                + channel.base_fee_millisatoshi
-                + (channel.fee_per_millionth * input.amount_msat),
+            input.amount_msat,
         );
 
         Ok(RouteOutput { path: vec![hop] })
